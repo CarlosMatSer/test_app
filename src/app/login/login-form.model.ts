d83fe2a62ac6d5269f71d181c86d0c6d77f9dfd4
CarlosMatSer <carlos.mateos@outlook.es>
2022-02-20 15:16:57 +0100

    feat(ts) creacion de formulario reactivo en login

---
+++ src/app/login/login-form.model.ts
@@ -0,0 +1,5 @@
+export interface formModel {
+    email?: string,
+    password?: string,
+    remember?: boolean
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { formModel } from './login-form.model';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,32 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  accessClicked = false;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  
+  createForm(){
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      remember: []
+    })
+  }
+
+  onSubmit(){
+    this.accessClicked = true;
+    if (this.loginForm.valid){
+      console.log('OK');
+    }
   }
 
 }
